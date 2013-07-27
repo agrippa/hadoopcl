2bbfc84b8839174456f21cd3ea0079ae6d7e69c3
Max Grossman <jmaxg3@gmail.com>
2013-07-27 01:06:00 -0500

    more work on value iterator

--- GenerateAll.py
+++ GenerateAll.py
@@ -3,10 +3,21 @@ import os
 
 fp = open('SupportedMR', 'r')
 
+generatedIters = [ ]
+
 for line in fp:
     cmd = 'python AutoGenerateKernel.py '+line[0:len(line)-1]
     print cmd
     os.system(cmd)
+
+    tokens = line.split()
+    if tokens[0] == 'reducer':
+        outputValType = tokens[4]
+        if not outputValType in generatedIters:
+            generatedIters.append(outputValType)
+            cmd = 'python AutoGenerateIter.py '+outputValType
+            print cmd
+            os.system(cmd)
 
 print
 arrayTypes = [ 'int', 'long', 'double', 'float' ]
