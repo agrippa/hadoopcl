fee6f32c3b6389e2542196d94ed725e4f655712f
Xiangyu Li <yiskylee@gmail.com>
2013-12-08 14:10:06 -0500

    replace with interface

--- core/org/apache/hadoop/io/ReadArrayUtils.java
+++ core/org/apache/hadoop/io/ReadArrayUtils.java
@@ -1,6 +1,7 @@
 package org.apache.hadoop.io;
 
-import org.apache.hadoop.fs.FSDataInputStream;
+import java.io.IOException;
+import java.io.DataInput;
 import java.nio.IntBuffer;
 import java.nio.FloatBuffer;
 import java.nio.DoubleBuffer;
@@ -8,49 +9,28 @@ import java.nio.ByteBuffer;
 
 public class ReadArrayUtils {
 
-    public static int[] readIntArray(FSDataInputStream input, final int len) throws IOException {
+    public static int[] readIntArray(DataInput input, final int len) throws IOException {
       final int totalBytes = len * 4;
       ByteBuffer byteBuffer = ByteBuffer.allocate(totalBytes);
-      int nRead = 0;
-      while (nRead < totalBytes) {
-        int current = input.read(byteBuffer.array(), nRead, totalBytes - nRead);
-        if (current < 0) {
-          throw new RuntimeException("Error reading stream during readIntArray");
-        }
-        nRead += current;
-      }
+      input.readFully(byteBuffer.array(), 0, totalBytes);
       int[] result = new int[len];
       byteBuffer.asIntBuffer().get(result);
       return result;
     }
 
-    public static float[] readFloatArray(FSDataInputStream input, final int len) throws IOException {
+    public static float[] readFloatArray(DataInput input, final int len) throws IOException {
       final int totalBytes = len * 4;
       ByteBuffer byteBuffer = ByteBuffer.allocate(totalBytes);
-      int nRead = 0;
-      while (nRead < totalBytes) {
-        int current = input.read(byteBuffer.array(), nRead, totalBytes - nRead);
-        if (current < 0) {
-          throw new RuntimeException("Error reading stream during readFloatArray");
-        }
-        nRead += current;
-      }
+      input.readFully(byteBuffer.array(), 0, totalBytes);
       float[] result = new float[len];
       byteBuffer.asFloatBuffer().get(result);
       return result;
     }
 
-    public static double[] readDoubleArray(FSDataInputStream input, final int len) throws IOException {
+    public static double[] readDoubleArray(DataInput input, final int len) throws IOException {
       final int totalBytes = len * 8;
       ByteBuffer byteBuffer = ByteBuffer.allocate(totalBytes);
-      int nRead = 0;
-      while (nRead < totalBytes) {
-        int current = input.read(byteBuffer.array(), nRead, totalBytes - nRead);
-        if (current < 0) {
-          throw new RuntimeException("Error reading stream during readDoubleArray");
-        }
-        nRead += current;
-      }
+      input.readFully(byteBuffer.array(), 0, totalBytes);
       double[] result = new double[len];
       byteBuffer.asDoubleBuffer().get(result);
       return result;
